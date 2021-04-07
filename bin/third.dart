23e778ee277af376e7187ce3ac68a7e51cd82f0f
salahgamal <81882072+salahgamal@users.noreply.github.com>
2021-04-07 05:08:07 +0200

    third task
    third task

---
+++ bin/third.dart
@@ -0,0 +1,5 @@
+import 'package:third/third.dart' as third;
+
+void main(List<String> arguments) {
+  print('Hello world: ${third.calculate()}!');
+}
---
+++ test/third_test.dart
@@ -0,0 +1,22 @@
+
+
+void main() {
+  //switch case
+  var num = 4;
+  switch (num) {
+    case 4:
+      print(4 + 4);
+      break;
+    case 5:
+      print(5 * 6);
+      break;
+    case 6:
+      print(10/2);
+      break;
+    case 1:
+      print(8-5);
+      break;
+      default:
+      print("error");
+  }
+}
